792051a696bbc9d5a21354f15d246d159ba968e2
lnunes1988 <nunes.ribeiro.lucas@gmail.com>
2021-01-24 14:13:25 -0300

    btc api

--- pages/api/btc.js
+++ pages/api/btc.js
@@ -5,7 +5,7 @@ async function bitcoin(resquest, response){
     const bitcoinResponse = await fetch("https://www.mercadobitcoin.net/api/BTC/ticker/")
     const bitcoinResponseJson = await bitcoinResponse.json();
     const ultimoPreco = bitcoinResponseJson.last;
-
+//ts
         response.json({
             date: dynamicDate.toGTMString(),
             valor: ultimoPreco
